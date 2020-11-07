724dc25ec7f0dbaff42a0da56df932af30915546
The-Jonsey <louis@thejonsey.com>
2020-11-07 15:06:27 +0000

    Added Handler interface

--- handler.go
+++ handler.go
@@ -2,7 +2,12 @@ package api
 
 import "image"
 
+type Handler interface {
+
+}
+
 type IconHandler interface {
+	Handler
 	Start(key Key, info StreamDeckInfo, callback func(image image.Image))
 	IsRunning() bool
 	SetRunning(running bool)
@@ -10,5 +15,6 @@ type IconHandler interface {
 }
 
 type KeyHandler interface {
+	Handler
 	Key(key Key, info StreamDeckInfo)
 }
