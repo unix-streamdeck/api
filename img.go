cc97d3a82ecba9d3c3b408ad5f5c9c2b888a1048
The-Jonsey <louis@thejonsey.com>
2026-04-06 15:04:37 +0100

    Rewrite text renderer

--- img.go
+++ img.go
@@ -26,6 +26,7 @@ import (
 	"golang.org/x/image/font/gofont/goregular"
 	"golang.org/x/image/font/gofont/gosmallcaps"
 	"golang.org/x/image/font/gofont/gosmallcapsitalic"
+	"golang.org/x/image/math/fixed"
 )
 
 const BorderClearance = 10
@@ -39,11 +40,28 @@ const (
 	Bottom VerticalAlignment = "BOTTOM"
 )
 
+type HorizontalAlignment string
+
+const (
+	Left   HorizontalAlignment = "LEFT"
+	Middle HorizontalAlignment = "MIDDLE"
+	Right  HorizontalAlignment = "RIGHT"
+)
+
+type Overflow string
+
+const (
+	Wrap Overflow = "WRAP"
+	Fade Overflow = "FADE"
+)
+
 type DrawTextOptions struct {
-	FontSize          int64
-	VerticalAlignment VerticalAlignment
-	FontFace          string
-	Colour            string
+	FontSize            int64
+	VerticalAlignment   VerticalAlignment
+	HorizontalAlignment HorizontalAlignment
+	FontFace            string
+	Colour              string
+	Overflow            Overflow
 }
 
 type IContext interface {
@@ -59,22 +77,31 @@ type IContext interface {
 }
 
 func DrawText(img image.Image, text string, options DrawTextOptions) (image.Image, error) {
-	ggImg := gg.NewContextForImage(img)
-	return drawText(ggImg, text, options)
-}
 
-func drawText(img IContext, text string, options DrawTextOptions) (image.Image, error) {
-	width, height := img.Width(), img.Height()
-	img.SetRGB(1, 1, 1)
+	if options.Overflow == "" {
+		options.Overflow = Wrap
+	}
+
+	drawImg, ok := img.(draw.Image)
+
+	if !ok {
+		return img, errors.New("cannot convert")
+	}
+
+	width, height := img.Bounds().Max.X, img.Bounds().Max.Y
+
+	col := color.RGBA{0xff, 0xff, 0xff, 0xff}
+
+	//img.SetRGB(1, 1, 1)
 	matched, _ := regexp.MatchString(`#?([0-9a-fA-F]{8}|[0-9a-fA-F]{6}|[0-9a-fA-F]{3})`, options.Colour)
 	if matched {
-		img.SetHexColor(options.Colour)
+		col = HexColor(options.Colour)
 	}
 	f, err := truetype.Parse(loadFontFace(options.FontFace))
 	if err != nil {
 		return nil, err
 	}
-	fSize := calculateFontSize(f, text, img)
+	fSize := calculateFontSize(f, text, img, options.Overflow)
 
 	if options.FontSize != 0 {
 		fSize = float64(options.FontSize)
@@ -82,18 +109,54 @@ func drawText(img IContext, text string, options DrawTextOptions) (image.Image, 
 
 	face := truetype.NewFace(f, &truetype.Options{Size: fSize})
 	defer face.Close()
-	img.SetFontFace(face)
 
-	lines := img.WordWrap(text, float64(width-BorderClearance))
-	lineCount := float64(len(lines))
+	lines := text
 
-	if strings.Contains(text, "\n") {
-		lineCount += float64(strings.Count(text, "\n") + 1)
+	if options.Overflow == Wrap {
+		lines = wrapString(text, width, face)
 	}
 
-	valign, y := calculateVerticalAlignment(options.VerticalAlignment, height)
-	img.DrawStringWrapped(text, float64(width/2), y, 0.5, valign, float64(width-BorderClearance), 1, gg.AlignCenter)
-	return img.Image(), nil
+	lineCount := strings.Count(lines, "\n") + 1
+
+	_, y := calculateVerticalAlignment(options.VerticalAlignment, height, lineCount, fSize)
+
+	d := &font.Drawer{
+		Dst:  drawImg,
+		Src:  image.NewUniform(col),
+		Face: face,
+	}
+
+	w, _ := getTextBounds(lines, face)
+
+	x := calculateHorizonalAlignment(options.HorizontalAlignment, int(w), width)
+
+	d.Dot = fixed.Point26_6{
+		X: fixed.I(x),
+		Y: fixed.I(int(y) + (int(fSize) / 4)),
+	}
+
+	linesSplit := strings.Split(lines, "\n")
+
+	if len(linesSplit) == 1 {
+		d.DrawString(lines)
+		return img, nil
+	}
+
+	linesAbove := float64(lineCount) / 2
+
+	linesAbove = linesAbove - 1
+
+	startingLineY := y - (linesAbove * fSize)
+
+	for i, line := range linesSplit {
+		w, _ := getTextBounds(line, face)
+		d.Dot = fixed.Point26_6{
+			X: fixed.I(calculateHorizonalAlignment(options.HorizontalAlignment, int(w), width)),
+			Y: fixed.I(int(startingLineY + (float64(i) * fSize))),
+		}
+		d.DrawString(line)
+	}
+	return img, nil
 }
 
 // TODO Support loading fonts via fontconfig on linux and whatever the equivalent is on darwin
@@ -128,29 +191,39 @@ func loadFontFace(fontName string) []byte {
 	}
 }
 
-func calculateVerticalAlignment(alignment VerticalAlignment, height int) (float64, float64) {
+func calculateVerticalAlignment(alignment VerticalAlignment, height, lines int, fSize float64) (float64, float64) {
+	textMidPoint := (float64(lines) / 2.0) * fSize
 	if alignment == Top {
-		return 0.0, BorderClearance / 2
+		return 0.0, (BorderClearance / 2) + (textMidPoint)
 	}
 	if alignment == Bottom {
-		return 1.0, float64(height) - (BorderClearance / 2)
+		return 1.0, float64(height) - (BorderClearance / 2) - textMidPoint
 	}
 	return 0.5, float64(height) / 2
 }
 
-func calculateFontSize(f *truetype.Font, text string, img IContext) float64 {
-	width := img.Width()
+func calculateHorizonalAlignment(alignment HorizontalAlignment, textWidth, width int) int {
+	if alignment == Left {
+		return BorderClearance / 2
+	}
+	if alignment == Right {
+		return width - (BorderClearance / 2) - textWidth
+	}
+	return ((width) / 2) - (int(textWidth) / 2)
+}
+
+func calculateFontSize(f *truetype.Font, text string, img image.Image, overflow Overflow) float64 {
+	width := img.Bounds().Dx()
 	fontSize := float64(width) / 3
 	face := truetype.NewFace(f, &truetype.Options{Size: fontSize})
 	defer face.Close()
-	img.SetFontFace(face)
-	textWidth, _ := img.MeasureMultilineString(text, 1.0)
+	w, _ := getTextBounds(text, face)
 	fSize := fontSize
-	if textWidth >= float64(width-BorderClearance) {
-		oversizeRatio := float64(width-BorderClearance) / textWidth
+	if w >= float64(width-BorderClearance) {
+		oversizeRatio := float64(width-BorderClearance) / w
 		scaledFontSize := math.Min(oversizeRatio*fontSize, 12)
 		for size := fontSize; size >= scaledFontSize; size -= 0.5 {
-			if attemptFontSize(f, text, img, size) {
+			if attemptFontSize(f, text, img, size, overflow) {
 				return size
 			}
 		}
@@ -159,16 +232,71 @@ func calculateFontSize(f *truetype.Font, text string, img IContext) float64 {
 	return fSize
 }
 
-func attemptFontSize(f *truetype.Font, text string, img IContext, fSize float64) bool {
-	width := img.Width()
-	height := img.Height()
+func attemptFontSize(f *truetype.Font, text string, img image.Image, fSize float64, overflow Overflow) bool {
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
 	face := truetype.NewFace(f, &truetype.Options{Size: fSize})
 	defer face.Close()
-	img.SetFontFace(face)
-	wrappedGroups := img.WordWrap(text, float64(width-BorderClearance))
-	wrappedText := strings.Join(wrappedGroups, "\n")
-	textWidth, textHeight := img.MeasureMultilineString(wrappedText, 1.0)
-	return textHeight < float64(height-BorderClearance) && textWidth < float64(width-BorderClearance)
+	w, h := getTextBounds(text, face)
+	if w <= float64(width-BorderClearance) {
+		return true
+	}
+	if h > float64(height) {
+		return false
+	}
+	if overflow != Wrap {
+		return false
+	}
+	lines := wrapString(text, width, face)
+	if lines == "" {
+		return false
+	}
+	maxTextWidth := 0.0
+	for _, s := range strings.Split(lines, "\n") {
+		textWidth, _ := getTextBounds(s, face)
+		if textWidth > maxTextWidth {
+			maxTextWidth = textWidth
+		}
+	}
+	textHeight := float64(strings.Count(lines, "\n")+1) * fSize
+	if textHeight < float64(height-BorderClearance) && maxTextWidth < float64(width-BorderClearance) {
+		return true
+	}
+	return false
+}
+
+func wrapString(text string, width int, face font.Face) string {
+	splitMessage := strings.Split(text, " ")
+	if len(splitMessage) == 1 {
+		return text
+	}
+
+	var lines []string
+	nextWordIndex := 0
+	for nextWordIndex < len(splitMessage) {
+		lineLength := 0.0
+		var line []string
+		for lineLength <= float64(width-BorderClearance) && nextWordIndex < len(splitMessage) {
+			w, _ := getTextBounds(splitMessage[nextWordIndex], face)
+			if w > float64(width-BorderClearance) {
+				return ""
+			}
+			if w+lineLength > float64(width-BorderClearance) {
+				break
+			}
+			lineLength += w
+			line = append(line, splitMessage[nextWordIndex])
+			nextWordIndex += 1
+		}
+		lines = append(lines, strings.Join(line, " "))
+	}
+	return strings.Join(lines, "\n")
+}
+
+func getTextBounds(text string, face font.Face) (float64, float64) {
+	bounds, _ := font.BoundString(face, text)
+
+	return (float64(bounds.Max.X.Round()) - float64(bounds.Min.X.Round())), (float64(bounds.Max.Y.Round()) - float64(bounds.Min.Y.Round()))
 }
 
 func ResizeImage(img image.Image, keySize int) image.Image {
